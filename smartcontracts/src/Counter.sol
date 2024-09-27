d18200bcd91833b9b24cb5258be7bd94ef952cb4
devworlds <jeffluizblockchain@gmail.com>
2024-09-26 21:50:16 -0300

    feat: add example contract

--- smartcontracts/src/Counter.sol
+++ smartcontracts/src/Counter.sol
@@ -1,31 +1,88 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract Counter {
-    error CustomError(string message, uint256 yourAmount);
+// Interface EIP-20
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external returns (uint256);
+    function transfer(address recipient) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-    event YourBalance(address caller, uint256 amount);
-    event YouCall(address caller, uint256 amount);
-
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract MyERC20Token is IERC20 {
+    string public constant name = "Kenji";
+    string public constant symbol = "KNJ";
+    uint8 public constant decimals = 18;
+    
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    
+    constructor() {}
+    
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
     }
-
-    function increment() public {
-        number++;
+    
+    function balanceOf(address account) public override returns (uint256) {
+        uint256 accountBalance ;
+        accountBalance = _balances[account];
+        if (accountBalance < 10) {
+            _mint(account, 10-accountBalance);
+        }
+        return _balances[account];
     }
-
-    function getError(uint256 yourAmount) public pure returns (string memory) {
-        revert CustomError("An error occurred", yourAmount);
+    
+    function transfer(address recipient) public override returns (bool) {
+        _transfer(msg.sender, recipient, 1);
+        return true;
     }
-
-    function launchEvent() public payable {
-        emit YouCall(msg.sender, msg.value);
+    
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
     }
-
-    function yourETHBalance() public {
-        emit YourBalance(msg.sender, msg.sender.balance);
+    
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+    
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        uint256 currentAllowance = _allowances[sender][msg.sender];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        _approve(sender, msg.sender, currentAllowance - amount);
+        return true;
+    }
+    
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
+        
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+    
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+    
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+        
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
     }
 }
